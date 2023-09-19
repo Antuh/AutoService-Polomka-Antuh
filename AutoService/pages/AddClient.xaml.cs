3a260442e25a04bf24f51d508c2b756a6f5c97be
Antuh <antuhsergey648@gmail.com>
2023-09-19 13:49:38 +0700

    Initial commit

---
+++ AutoService/pages/AddClient.xaml.cs
@@ -0,0 +1,130 @@
+﻿using AutoService.model;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AutoService.pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AddClient.xaml
+    /// </summary>
+    public partial class AddClient : Page
+    {
+        Client client = new Client();
+
+        private bool _editClient = false;
+
+        public AddClient(Client currentClient)
+        {
+            InitializeComponent();
+            if (currentClient != null)
+            {
+                _editClient = true;
+                client = currentClient;
+                btnDeleteClient.Visibility = Visibility.Visible;
+            }
+            DataContext = client;
+            txtGender.ItemsSource = EntitiesAutos.GetContext().Gender.ToList();
+        }
+
+        private void btnEnterImage_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                OpenFileDialog GetImageDialog = new OpenFileDialog();
+
+                GetImageDialog.Filter = "Файлы изображений: (*.png, *.jpg, *jpeg)| *.png; *.jpg; *jpeg";
+                GetImageDialog.InitialDirectory = "AutoService\\image\\Клиенты";
+                if (GetImageDialog.ShowDialog() == true) client.PhotoPath = GetImageDialog.SafeFileName;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void btnDeleteClient_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show($"Вы жействительно хотите удалить {client}?", "Внимание", MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.Yes)
+            {
+                try
+                {
+                    EntitiesAutos.GetContext().Client.Remove(client);
+                    EntitiesAutos.GetContext().SaveChanges();
+                    MessageBox.Show("Запись удалена!", "Информация", MessageBoxButton.OK, MessageBoxImage.Information);
+                    NavigationService.GoBack();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString(), "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+        private bool IsLettersOnly(string input)
+        {
+            // Используем регулярное выражение для проверки, что строка состоит только из букв
+            return !string.IsNullOrEmpty(input) && Regex.IsMatch(input, "^[a-zA-Z]+$");
+        }
+
+        private void btnSaveClient_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtFirstName.Text) ||
+            string.IsNullOrEmpty(txtLastName.Text) ||
+            string.IsNullOrEmpty(txtPatronymic.Text) ||
+            string.IsNullOrEmpty(txtEmail.Text) ||
+            string.IsNullOrEmpty(txtPhone.Text) ||
+            txtGender.SelectedItem == null ||
+            dtbirth.SelectedDate == null)
+            {
+                MessageBox.Show("Не все поля заполнены");
+            }
+            else if (!IsLettersOnly(txtFirstName.Text) || !IsLettersOnly(txtPatronymic.Text))
+            {
+                MessageBox.Show("Имя и отчество должны содержать только буквы");
+            }
+            else
+            {
+                if (_editClient == false)
+                {
+                    
+                    var client = new Client
+                    {
+                        FirstName = txtFirstName.Text,
+                        LastName = txtLastName.Text,
+                        Patronymic = txtPatronymic.Text,
+                        Email = txtEmail.Text,
+                        Phone = txtPhone.Text,
+                        Gender = txtGender.SelectedItem as Gender,
+                        Birthday = dtbirth.SelectedDate,
+                        RegistrationDate = DateTime.Now
+                    };
+
+                    EntitiesAutos.GetContext().Client.Add(client);
+                }
+
+                try
+                {
+                    EntitiesAutos.GetContext().SaveChanges();
+                    MessageBox.Show("Информация сохранена", "Информация", MessageBoxButton.OK, MessageBoxImage.Information);
+                    NavigationService.GoBack();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString(), "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+    }
+}
---
+++ AutoService/pages/ClientsAutoService.xaml.cs
@@ -0,0 +1,118 @@
+﻿using AutoService.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+/*
+ * private static EntitiesAuto _context;
+
+        public static EntitiesAuto GetContext()
+        {
+            if (_context == null)
+                _context = new EntitiesAuto();
+            return _context;
+        }
+ */
+/*
+ * public string ImgPath
+        {
+            get
+            {
+                var path = "pack://application:,,,/image/" + this.PhotoPath;
+                return path;
+            }
+        }
+ */
+
+namespace AutoService.pages
+{
+    /// <summary>
+    /// Логика взаимодействия для ClientsAutoService.xaml
+    /// </summary>
+    public partial class ClientsAutoService : Page
+    {
+        public ClientsAutoService()
+        {
+            InitializeComponent();
+            var client = EntitiesAutos.GetContext().Client.ToList();
+            LViewClient.ItemsSource = client;
+            DataContext = this;
+        }
+        public string[] SortingList { get; set; } =
+        {
+            "Без сортировки",
+            "По возрастанию алфавита",
+            "По убыванию алфавита"
+        };
+        public string PhotoPath { get; internal set; }
+
+        private void UpdateData()
+        {
+            var result = EntitiesAutos.GetContext().Client.ToList();
+
+            if (cmbSorting.SelectedIndex == 1) result = result.OrderBy(p => p.LastName).ToList();
+            if (cmbSorting.SelectedIndex == 2) result = result.OrderByDescending(p => p.LastName).ToList();
+
+            /*if (cmbFilter.SelectedIndex == 1) result = result.Where(p => p.ProductDiscountAmount >= 0 && p.ProductDiscountAmount < 10).ToList();;*/
+
+            result = result.Where(p =>
+            p.FirstName.ToLower().Contains(txtSearch.Text.ToLower()) ||
+            p.LastName.ToLower().Contains(txtSearch.Text.ToLower()) ||
+            p.Phone.ToLower().Contains(txtSearch.Text.ToLower()) ||
+            p.Patronymic.ToLower().Contains(txtSearch.Text.ToLower())
+            ).ToList();
+            LViewClient.ItemsSource = result;
+            txtResultAmount.Text = result.Count().ToString();
+        }
+
+        private void btnOrder_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnAddProduct_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void cmbSorting_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateData();
+        }
+
+        private void cmbFilter_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateData();
+        }
+
+        private void txtSearch_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            UpdateData();
+        }
+
+        private void btnEditClient_Click(object sender, RoutedEventArgs e)
+        {
+            /*Client selectedClient = (sender as Button).DataContext as Client;
+
+            if (selectedClient != null)
+            {
+                NavigationService.Navigate(new EditClient(selectedClient));
+            }*/
+        }
+
+        private void btnAddNewClient_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new AddClient(null));
+        }
+    }
+}
